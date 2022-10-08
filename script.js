d8bf427ebd93075b716b09c07c3b3d922d7d31ae
GunnDroid <glukari@protonmail.com>
2022-10-07 23:14:04 -0500

    finished light/dark

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function myFunction() {
+  // change body background and paragraph text toggle between dark mode on/off
   let element = document.body;
-
   element.classList.toggle("dark-mode");
 
+  // Text change
   if (document.getElementById("lightDark").innerHTML === "Dark Mode") {
     document.getElementById("lightDark").innerHTML = "Light Mode";
   } else {
